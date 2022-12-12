f770684dafc89fdcdfe443a4a90422b5e4e29239
blackoaz <pauloakello56@gmail.com>
2022-12-12 06:03:03 -0800

    ommit letter e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * *main-program to print alphabet letters except q and e followed by new line
+ *
+ * Return: retun 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return(0);
+}

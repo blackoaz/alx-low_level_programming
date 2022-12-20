620afc161c8bc9665c15bdf8166aabc34329951e
blackoaz <pauloakello56@gmail.com>
2022-12-20 04:43:22 -0800

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@ void rev_string(char *s)
 {
 	char tmp;
 	int i, len, len1;
+	
 	len = 0;
 	len1 = 0;
 
@@ -18,7 +19,7 @@ void rev_string(char *s)
 
 	len1  = len - 1;
 
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[len1];

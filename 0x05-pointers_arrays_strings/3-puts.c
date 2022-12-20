43ebe30f250d25d985edcef3ed12e9b8f1e3fecd
blackoaz <pauloakello56@gmail.com>
2022-12-20 04:20:22 -0800

    printing a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,10 @@
-#include<main.h>
+#include "main.h"
 /**
- * _puts - prints a string
+ *_puts - prints a string
+ * @str: string to print
  *
  * Description: prints a string
- * on success: return the number of characters printed
+ * On success: return the number of characters printed
  */
 
 void _puts(char *str)
@@ -11,5 +12,5 @@ void _puts(char *str)
 while (*str)
 _putchar(*str++);
 
-_putchar('\n')
+_putchar('\n');
 }

79bd7b76cc3bfc1f94ed8dde7cb344b0c8778198
blackoaz <pauloakello56@gmail.com>
2022-12-20 04:02:53 -0800

    Reseting value using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main-reset value to 98
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int n = 0 ;
-	int *p = &n;
-	*p = 98;
-	printf("/d\n", n);
-	return(0);
-	
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
 
+void reset_to_98(int *n)
+{
+*n = 98;
 }

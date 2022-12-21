f79a3de53c54e7fa008790e6dea19ab17708ada8
blackoaz <pauloakello56@gmail.com>
2022-12-20 23:54:31 -0800

    copying two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncpy - c function copying a string including the terminating null byte
+ * Returns: return
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}

ccf132dccc08aa0fd73a4066952753f2833c6f61
blackoaz <pauloakello56@gmail.com>
2022-12-21 20:37:42 -0800

    updates for 0x-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
- *
- * *_strcat - function that concatenates two strings.
- *
- * *@dest: pointer to destination string.
- *
- * *@src: pointer to source string.
- *
- * *
- *
- * *Return: pointer to destination string.
- *
+ * _strcat - function that concatenates two strings.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *Return: pointer to destination string.
  */
 
 char *_strcat(char *dest, char *src)

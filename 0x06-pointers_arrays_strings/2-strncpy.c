b6f0100b6b574183b32594333041fbc4942b39a3
blackoaz <pauloakello56@gmail.com>
2022-12-21 21:50:29 -0800

    update to task no 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
  * _strncpy - c function copying a string including the terminating null byte
- * Returns: return
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: number of bytes to be used
+ *
+ * Returns: pointer to destination string
  */
 char *_strncpy(char *dest, char *src, int n)
 {

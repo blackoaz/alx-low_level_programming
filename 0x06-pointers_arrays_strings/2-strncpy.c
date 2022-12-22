6859d79a4cff65fe31c2586979add154db567773
blackoaz <pauloakello56@gmail.com>
2022-12-21 22:06:43 -0800

    function for copying

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _strncpy - c function copying a string including the terminating null byte
- * @dest: pointer to destination string
- * @src: pointer to source string
- * @n: number of bytes to be used
+ * _strncpy - a function that copies a string.
+ *at most n bytes of src are copied.
+ *if length of src is < n, additional null bytes are written to dest
+ *to ensure that a total of n bytes are written.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be used.
  *
- * Returns: pointer to destination string
+ * Return: pointer to destination string.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int numByte;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for (; i < n; i++)
-		dest[i] = '\0';
+	for (numByte = 0; numByte < n && src[numByte] != '\0'; numByte++)
+		dest[numByte] = '\0';
 
 	return (dest);
 }

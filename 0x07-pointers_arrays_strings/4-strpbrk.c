65318355dc38f0cc5c065326f30a85edf989ce9a
blackoaz <pauloakello56@gmail.com>
2023-01-03 23:22:48 -0800

    adding task 4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,20 @@
 *Return:Returns a pointer to the byte in s that matches
 * one of the bytes in accept, or NULL if no such byte is found
 */
+
 char *_strpbrk(char *s, char *accept)
 {
-	for (; *s != '\0'; s++)
+	int i;
+
+	while (*s)
 	{
-		for (char *a = accept; *a != '\0'; a++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == *a)
-			{
+			if (accept[i] == *s)
 				return (s);
-			}
 		}
+		s++;
 	}
-	return (NULL);
+
+	return ('\0');
 }

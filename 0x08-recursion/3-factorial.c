39178e20c7a171b92200f9d2ece784c7f335e92b
blackoaz <pauloakello56@gmail.com>
2023-01-05 22:42:38 -0800

    adding task 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,9 +8,13 @@
  */
 int factorial(int n)
 {
+	int val;
+
 	if (n == 0)
 		return (1);
 	else if (n < 0)
-		return (-1);
-	return (n * (n - 1));
+	return (-1);
+
+	val = factorial(n - 1);
+	return (n * val);
 }

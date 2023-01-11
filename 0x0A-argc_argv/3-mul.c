02257e2d9041a8e92d01bf00b6888f412473ecd3
blackoaz <pauloakello56@gmail.com>
2023-01-10 22:55:00 -0800

    adding task 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, mul;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	mul = j(atoi(argv[1])) * i(atoi(argv[2]);
+
+	printf("%d", mul);
+	return (0)
+}

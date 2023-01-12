1101a0db297960607a80ec0eb3ba5b0f4bfaf4de
blackoaz <pauloakello56@gmail.com>
2023-01-12 00:50:33 -0800

    adding main.h, gitignire files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * void prototypes(void)
+ * int prototypes(int)
+ * char prototypes(char)
+ */
+char *create_array(unsigned int size, char c);
+
+#endif /*MAIN_H*/

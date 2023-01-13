eab4a69c7004c32d992266b522d272c84ced1ec7
blackoaz <pauloakello56@gmail.com>
2023-01-13 09:07:09 -0800

    adding task 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,9 +11,9 @@ void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
-	ptr = (int *)malloc(sizeof(int) * b)
+	ptr = malloc(b)
 		if (ptr == NULL)
 			exit(98);
-	return (*ptr);
+	return (ptr);
 
 }

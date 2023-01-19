d92bd60d82f2d23a5ed3922c07ae00158d67a0b9
blackoaz <pauloakello56@gmail.com>
2023-01-19 06:59:51 -0800

    adding task 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,12 +7,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
 		return;
 
-	while (size-> 0)
-	{
-		action(*array);
-		array++;
-	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }

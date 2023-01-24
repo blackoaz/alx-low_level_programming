d8ce471f6ffe8ec73aabd5e08bfdd56eb17a2fa3
blackoaz <pauloakello56@gmail.com>
2023-01-24 11:22:54 -0800

    adding updates on variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,10 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
-/**
- * int prototype(int)
- * char prototype(char)
- * void prototype(void)
- */
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /*VARIADIC_FUNCTION_H*/
+#endif

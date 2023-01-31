782a5ed90ee1387068b4340ceee4c5ab8591b6f4
blackoaz <pauloakello56@gmail.com>
2023-01-31 07:43:46 -0800

    adding task 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,25 +7,26 @@
  * @str:string to be duplicated
  * Return: returns the address of the new element or NULL if failed
  */
-list_t *add_node(list **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	int i, count = 0;
 	list_t *new;
 
 	if (head == NULL)
 		return (NULL);
-	new = malloc(sizeof(list_t));
 
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
+
 	new->len = i;
 	new->str = strdup(str);
-	new->next = head;
+	new->next = *head;
 
 	*head = new;
 
 	return (new);
-
 }

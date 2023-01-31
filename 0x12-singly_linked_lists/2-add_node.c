cebc8a552f6af30a6e01fa69119a29852745d88b
blackoaz <pauloakello56@gmail.com>
2023-01-31 07:37:55 -0800

    adding task 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *add_node - function that adds node to the behggining of a list
+ * @head: the struct
+ * @str:string to be duplicated
+ * Return: returns the address of the new element or NULL if failed
+ */
+list_t *add_node(list **head, const char *str)
+{
+	int i, count = 0;
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	new->len = i;
+	new->str = strdup(str);
+	new-> = *head;
+
+	*head = new;
+
+	return (new);
+
+}

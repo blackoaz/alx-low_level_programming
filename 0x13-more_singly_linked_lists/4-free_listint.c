8c369eec1b8cad07f4c58f2b1448c2fd9ef40eb7
blackoaz <pauloakello56@gmail.com>
2023-02-02 06:32:10 -0800

    adding task 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-		return;
+    listint_t *temp;
 
-	while (head)
-	{
-		free(head);
-		head = head->next;
-	}
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }

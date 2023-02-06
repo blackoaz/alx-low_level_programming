ce4582c8339bcbcc3ac9174bf63ca555c40b04c7
blackoaz <pauloakello56@gmail.com>
2023-02-06 12:21:38 -0800

    adding task 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,26 +6,22 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		return (0);
-	unsigned long int pow = 1;
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
 
-	while (pow <= n / 2)
-	{
-		pow *= 2;
-	}
-while (pow > 0)
-{
-	if (n >= pow)
-	{
-		putchar ('1');
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
 
-		n -= pow;
+	if (printbit == 0)
+		write(1, &c, 1);
 
-	}
-	else
+	while (printbit)
 	{
-		putchar ('0');
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
 	}
-	pow /= 2;
 }

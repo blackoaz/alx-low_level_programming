b0a4baff216fec03f28af055b6ae19d508c4263f
blackoaz <pauloakello56@gmail.com>
2023-02-09 07:21:06 -0800

    adding task 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * create_file - function for creating a file
+ * @filename: name of file
+ * @text_content: content for appending
+ * Return: returns 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int o_rw = 0, length = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREATE | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		o_rw = write(file, text_content, sizeof(text_content));
+	}
+	close(file);
+	return (1);
+}

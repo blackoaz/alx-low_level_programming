09ebc11e1ff44be74431d54a5447eff6e938a3c2
blackoaz <pauloakello56@gmail.com>
2023-02-09 07:31:28 -0800

    adding task 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,22 +13,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	int o_rw = 0, length = 0;
+	int length = 0, inlen = 0;
 	char *ptr;
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREATE | O_TRUNC, 0600);
 
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (length = 0; ptr = text_content; *ptr; ptr++)
-			length++;
-		o_rw = write(file, text_content, length);
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
 	}
-	if (close(file) == -1 || length != o_rw)
+
+	if (close(file) == -1 || inlen != length)
 		return (-1);
 	return (1);
 }

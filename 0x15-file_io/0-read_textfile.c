5b2cf0ecebe93cfa16effa53c0ddebeabf3b7bed
blackoaz <pauloakello56@gmail.com>
2023-02-09 06:38:35 -0800

    adding task 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,29 +8,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	int o_rd, o_rw;
 	size_t read_bytes, written_bytes;
 	char *buffer;
 
-	if (filename == -1)
+	if (filename == NULL)
 		return (0);
+	buffer = malloc(sizeof(char)*letters);
 
 	fd = open(filename, O_RDONLY);
 
-	if (read_bytes == -1)
+	if (fd == -1)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+
+	o_rd = read(filename, buffer, letters);
+
+	if (o_rd == -1)
 		return (0);
-	read_bytes = write(STDOUT_FILE, buffer, read_bytes);
-	if (written_bytes == -1 || written_bytes != read_bytes)
-	{
-		free(buffer);
+
+	o_rw = write(filename, buffer, o_rd);
+
+	if (o_rw == -1)
 		return (0);
-	}
 	free(buffer);
 	close(fd);
-	return (read_bytes);
+	return(o_rd);
+
 
 }

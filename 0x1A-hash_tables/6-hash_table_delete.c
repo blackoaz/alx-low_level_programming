5f74f7d2a0dd96ec831070e203d03a7aa90a54f3
blackoaz <pauloakello56@gmail.com>
2023-04-12 01:43:24 -0700

    adding task 7

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -13,7 +13,7 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	for (i = 0; i , ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node != NULL)
@@ -21,7 +21,7 @@ void hash_table_delete(hash_table_t *ht)
 			temp = node;
 			node = node->next;
 			free(temp->key);
-			free(tmp->value);
+			free(temp->value);
 			free(temp);
 		}
 	}
